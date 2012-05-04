e0c1a3c15a3d425a36b6cf49e8577be62d075071
Christian Berkhoff <christian.berkhoff@gmail.com>
2012-05-04 08:50:14 -0400

    having fun with the grid

--- app/application.coffee
+++ app/application.coffee
@@ -1,27 +1,141 @@
 Router = require 'lib/router'
-Miner = require 'views/miner'
+
+gridWidth = 5
+gridHeight = 5
+viewportWidth = 9
+viewportHeight = 7
+cellSize = 40
+
+# x,y son para el dibujo
+# i,j son posiciones 2d
+# k es un indice
+
+class Position
+  # The position in the grid
+  constructor: (@i, @j) ->
+  x: -> @i * cellSize
+  y: -> @j * cellSize
+
+class Cell
+  constructor: (@pos, @paper) ->
+  draw: ->
+    @rect = @paper.rect @pos.x(), @pos.y(), cellSize, cellSize
+
+class AirCell extends Cell
+  draw: ->
+    super()
+    @rect.attr 'fill', '#C0E5E4'
+
+class SoftEarthCell extends Cell
+  draw: ->
+    super()
+    @rect.attr 'fill', '#B99B64'
+
+class EarthCell extends Cell
+  draw: ->
+    super()
+    @rect.attr 'fill', '#9B8569'
+
+class HardEarthCell extends Cell
+  draw: ->
+    super()
+    @rect.attr 'fill', '#786B66'
+
+class MetalCell extends Cell
+  draw: ->
+    super()
+    @rect.attr 'fill', '#784F5F'
+
+class Grid
+  constructor: (@width, @height, @paper) ->
+    @bounds = new Position @width, @height
+    @cells = []
+
+  indexToPosition: (k)->
+    j = Math.floor k / @width
+    i = k - j * @width
+    new Position i, j
+
+  positionToIndex: (pos) ->
+    pos.i * @width + pos.j
+
+  setCell: (c) ->
+    @cells[@positionToIndex c.pos] = c
+
+  parse: (grid) ->
+    for k in [0...grid.length]
+      switch grid[k]
+        when 'a' then @setCell new AirCell @indexToPosition k, @paper
+        when 's' then @setCell new SoftEarthCell @indexToPosition k, @paper
+        when 'e' then @setCell new EarthCell @indexToPosition k, @paper
+        when 'h' then @setCell new HardEarthCell @indexToPosition k, @paper
+        when 'm' then @setCell new MetalCell @indexToPosition k, @paper
+
+  draw: ->
+    @cells.map (c) ->
+      c.draw
+
+
+class Miner
+  constructor: (@pos, @paper) ->
+    @draw()
+
+  draw: ->
+    h = cellSize / 2
+    @circle = @paper.circle @pos.x() - h, @pos.y() - h, h
+    @circle.attr "fill", "#f00"
+
+  drawMove: (dir) ->
+    attr = @circle.attr()
+    switch dir
+      when 'up' then attr.cy -= cellSize
+      when 'down' then attr.cy += cellSize
+      when 'left' then attr.cx -= cellSize
+      when 'right' then attr.cx += cellSize
+    @circle.animate attr, 200
+
+  handleKeydown: (kc) ->
+    switch kc
+      when 38 then @drawMove 'up'
+      when 40 then @drawMove 'down'
+      when 37 then @drawMove 'left'
+      when 39 then @drawMove 'right'
 
 # The application bootstrapper.
 class Application
+  player: undefined
   initialize: ->
-    @paper = Raphael "canvas", 640, 480
+    viewportBounds = new Position viewportWidth, viewportHeight
+
+    @paper = Raphael "canvas", viewportBounds.x(), viewportBounds.y()
+
+    grid = new Grid gridWidth, gridHeight, @paper
+    sample_grid = [
+      'a', 'a', 'a', 'a', 'a'
+      's', 's', 's', 's', 's'
+      'e', 'e', 's', 'h', 's'
+      'h', 'e', 'e', 'h', 'e'
+      'e', 'e', 'e', 'e', 'e'
+      'm', 'm', 'm', 'e', 'e'
+    ]
+    grid.parse sample_grid
+    console.log grid.cells
+    grid.draw()
+    # hacer esto con un mapa que se note
+    #@paper.setViewBox 0, 0, viewportBounds.x(), viewportBounds.y(), true
 
     # Ideally, initialized classes should be kept in controllers & mediator.
     # If you're making big webapp, here's more sophisticated skeleton
     # https://github.com/paulmillr/brunch-with-chaplin
 
-    @players = []
-
     # Instantiate the router
-    @router = new Router()
-    # Freeze the object
-    Object.freeze? this
+    #@router = new Router()
+    @joinGame()
 
-  newPlayer: (name) ->
-    m = new Miner @paper
-    @players.push m
-    m.render()
-
+  joinGame: ->
+    @player = new Miner(new Position(3, 2), @paper)
+    $(document).keydown (kde) =>
+      @player.handleKeydown kde.keyCode
 
 app = new Application
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -81,28 +81,278 @@
 (this.require.define({
   "application": function(exports, require, module) {
     (function() {
-  var Application, Miner, Router, app;
+  var AirCell, Application, Cell, EarthCell, Grid, HardEarthCell, MetalCell, Miner, Position, Router, SoftEarthCell, app, cellSize, gridHeight, gridWidth, viewportHeight, viewportWidth,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   Router = require('lib/router');
 
-  Miner = require('views/miner');
+  gridWidth = 5;
+
+  gridHeight = 5;
+
+  viewportWidth = 9;
+
+  viewportHeight = 7;
+
+  cellSize = 40;
+
+  Position = (function() {
+
+    function Position(i, j) {
+      this.i = i;
+      this.j = j;
+    }
+
+    Position.prototype.x = function() {
+      return this.i * cellSize;
+    };
+
+    Position.prototype.y = function() {
+      return this.j * cellSize;
+    };
+
+    return Position;
+
+  })();
+
+  Cell = (function() {
+
+    function Cell(pos, paper) {
+      this.pos = pos;
+      this.paper = paper;
+    }
+
+    Cell.prototype.draw = function() {
+      return this.rect = this.paper.rect(this.pos.x(), this.pos.y(), cellSize, cellSize);
+    };
+
+    return Cell;
+
+  })();
+
+  AirCell = (function(_super) {
+
+    __extends(AirCell, _super);
+
+    function AirCell() {
+      AirCell.__super__.constructor.apply(this, arguments);
+    }
+
+    AirCell.prototype.draw = function() {
+      AirCell.__super__.draw.call(this);
+      return this.rect.attr('fill', '#C0E5E4');
+    };
+
+    return AirCell;
+
+  })(Cell);
+
+  SoftEarthCell = (function(_super) {
+
+    __extends(SoftEarthCell, _super);
+
+    function SoftEarthCell() {
+      SoftEarthCell.__super__.constructor.apply(this, arguments);
+    }
+
+    SoftEarthCell.prototype.draw = function() {
+      SoftEarthCell.__super__.draw.call(this);
+      return this.rect.attr('fill', '#B99B64');
+    };
+
+    return SoftEarthCell;
+
+  })(Cell);
+
+  EarthCell = (function(_super) {
+
+    __extends(EarthCell, _super);
+
+    function EarthCell() {
+      EarthCell.__super__.constructor.apply(this, arguments);
+    }
+
+    EarthCell.prototype.draw = function() {
+      EarthCell.__super__.draw.call(this);
+      return this.rect.attr('fill', '#9B8569');
+    };
+
+    return EarthCell;
+
+  })(Cell);
+
+  HardEarthCell = (function(_super) {
+
+    __extends(HardEarthCell, _super);
+
+    function HardEarthCell() {
+      HardEarthCell.__super__.constructor.apply(this, arguments);
+    }
+
+    HardEarthCell.prototype.draw = function() {
+      HardEarthCell.__super__.draw.call(this);
+      return this.rect.attr('fill', '#786B66');
+    };
+
+    return HardEarthCell;
+
+  })(Cell);
+
+  MetalCell = (function(_super) {
+
+    __extends(MetalCell, _super);
+
+    function MetalCell() {
+      MetalCell.__super__.constructor.apply(this, arguments);
+    }
+
+    MetalCell.prototype.draw = function() {
+      MetalCell.__super__.draw.call(this);
+      return this.rect.attr('fill', '#784F5F');
+    };
+
+    return MetalCell;
+
+  })(Cell);
+
+  Grid = (function() {
+
+    function Grid(width, height, paper) {
+      this.width = width;
+      this.height = height;
+      this.paper = paper;
+      this.bounds = new Position(this.width, this.height);
+      this.cells = [];
+    }
+
+    Grid.prototype.indexToPosition = function(k) {
+      var i, j;
+      j = Math.floor(k / this.width);
+      i = k - j * this.width;
+      return new Position(i, j);
+    };
+
+    Grid.prototype.positionToIndex = function(pos) {
+      return pos.i * this.width + pos.j;
+    };
+
+    Grid.prototype.setCell = function(c) {
+      return this.cells[this.positionToIndex(c.pos)] = c;
+    };
+
+    Grid.prototype.parse = function(grid) {
+      var k, _ref, _results;
+      _results = [];
+      for (k = 0, _ref = grid.length; 0 <= _ref ? k < _ref : k > _ref; 0 <= _ref ? k++ : k--) {
+        switch (grid[k]) {
+          case 'a':
+            _results.push(this.setCell(new AirCell(this.indexToPosition(k, this.paper))));
+            break;
+          case 's':
+            _results.push(this.setCell(new SoftEarthCell(this.indexToPosition(k, this.paper))));
+            break;
+          case 'e':
+            _results.push(this.setCell(new EarthCell(this.indexToPosition(k, this.paper))));
+            break;
+          case 'h':
+            _results.push(this.setCell(new HardEarthCell(this.indexToPosition(k, this.paper))));
+            break;
+          case 'm':
+            _results.push(this.setCell(new MetalCell(this.indexToPosition(k, this.paper))));
+            break;
+          default:
+            _results.push(void 0);
+        }
+      }
+      return _results;
+    };
+
+    Grid.prototype.draw = function() {
+      return this.cells.map(function(c) {
+        return c.draw;
+      });
+    };
+
+    return Grid;
+
+  })();
+
+  Miner = (function() {
+
+    function Miner(pos, paper) {
+      this.pos = pos;
+      this.paper = paper;
+      this.draw();
+    }
+
+    Miner.prototype.draw = function() {
+      var h;
+      h = cellSize / 2;
+      this.circle = this.paper.circle(this.pos.x() - h, this.pos.y() - h, h);
+      return this.circle.attr("fill", "#f00");
+    };
+
+    Miner.prototype.drawMove = function(dir) {
+      var attr;
+      attr = this.circle.attr();
+      switch (dir) {
+        case 'up':
+          attr.cy -= cellSize;
+          break;
+        case 'down':
+          attr.cy += cellSize;
+          break;
+        case 'left':
+          attr.cx -= cellSize;
+          break;
+        case 'right':
+          attr.cx += cellSize;
+      }
+      return this.circle.animate(attr, 200);
+    };
+
+    Miner.prototype.handleKeydown = function(kc) {
+      switch (kc) {
+        case 38:
+          return this.drawMove('up');
+        case 40:
+          return this.drawMove('down');
+        case 37:
+          return this.drawMove('left');
+        case 39:
+          return this.drawMove('right');
+      }
+    };
+
+    return Miner;
+
+  })();
 
   Application = (function() {
 
     function Application() {}
 
+    Application.prototype.player = void 0;
+
     Application.prototype.initialize = function() {
-      this.paper = Raphael("canvas", 640, 480);
-      this.players = [];
-      this.router = new Router();
-      return typeof Object.freeze === "function" ? Object.freeze(this) : void 0;
+      var grid, sample_grid, viewportBounds;
+      viewportBounds = new Position(viewportWidth, viewportHeight);
+      this.paper = Raphael("canvas", viewportBounds.x(), viewportBounds.y());
+      grid = new Grid(gridWidth, gridHeight, this.paper);
+      sample_grid = ['a', 'a', 'a', 'a', 'a', 's', 's', 's', 's', 's', 'e', 'e', 's', 'h', 's', 'h', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'e', 'e', 'm', 'm', 'm', 'e', 'e'];
+      grid.parse(sample_grid);
+      console.log(grid.cells);
+      grid.draw();
+      return this.joinGame();
     };
 
-    Application.prototype.newPlayer = function(name) {
-      var m;
-      m = new Miner(this.paper);
-      this.players.push(m);
-      return m.render();
+    Application.prototype.joinGame = function() {
+      var _this = this;
+      this.player = new Miner(new Position(3, 2), this.paper);
+      return $(document).keydown(function(kde) {
+        return _this.player.handleKeydown(kde.keyCode);
+      });
     };
 
     return Application;
